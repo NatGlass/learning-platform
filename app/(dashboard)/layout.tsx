8b32282848be7acf57d6a74f1f482456c1b62b29
Visibyte <nathanglass26@gmail.com>
2023-09-25 20:00:19 +0100

    sidebar

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,13 @@
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full">
+      {/* large screen sidebar */}
+      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+        <Sidebar />
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default DashboardLayout;
