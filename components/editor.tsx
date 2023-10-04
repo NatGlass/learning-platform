5fc498b2dc0dee80322a238e4b66185ce9583c2d
Visibyte <nathanglass26@gmail.com>
2023-10-04 13:49:23 +0100

    chapter access

---
+++ components/editor.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+// This files purpose is to import React Quill without using server-side rendering
+// SSR will cause hydration issues
+
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+
+import "react-quill/dist/quill.snow.css";
+
+interface EditorProps {
+  onChange: (value: string) => void;
+  value: string;
+}
+
+const Editor = ({ onChange, value }: EditorProps) => {
+  const ReactQuill = useMemo(
+    () => dynamic(() => import("react-quill"), { ssr: false }),
+    []
+  );
+
+  return (
+    <div className="bg-white">
+      <ReactQuill theme="snow" value={value} onChange={onChange} />
+    </div>
+  );
+};
+
+export default Editor;
