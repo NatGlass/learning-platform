84a3e48005a8a4e4f9f35a96b36a160fb19db281
Visibyte <nathanglass26@gmail.com>
2023-10-08 19:38:34 +0100

    video player

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import axios from "axios";
+import MuxPlayer from "@mux/mux-player-react";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { Lock, Loader2 } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { useConfettiStore } from "@/hooks/use-confetti-store";
+
+interface VideoPlayerProps {
+  chapterId: string;
+  title: string;
+  courseId: string;
+  nextChapterId?: string;
+  playbackId: string;
+  isLocked: boolean;
+  completeOnEnd: boolean;
+}
+
+const VideoPlayer = ({
+  chapterId,
+  title,
+  courseId,
+  nextChapterId,
+  playbackId,
+  isLocked,
+  completeOnEnd,
+}: VideoPlayerProps) => {
+  const [isReady, setIsReady] = useState(false);
+  return (
+    <div className="relative aspect-video">
+      {!isReady && !isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
+          <Loader2 className="h-8 w-8 animate-spin text-secondary" />
+        </div>
+      )}
+      {isLocked && (
+        <div className="absolute inset-0 flex flex-col gap-y-2 items-center justify-center bg-slate-800">
+          <Lock className="h-8 w-8 text-white" />
+          <p className="text-sm text-white">This chapter is locked</p>
+        </div>
+      )}
+      {!isLocked && (
+        <MuxPlayer
+          title={title}
+          className={cn(!isReady && "hidden")}
+          onCanPlay={() => setIsReady(true)}
+          onEnded={() => {}}
+          autoPlay
+          playbackId={playbackId}
+        />
+      )}
+    </div>
+  );
+};
+
+export default VideoPlayer;
