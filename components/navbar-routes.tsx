fd065ecf479b622f515a64e2f295bb108c103ccc
Visibyte <nathanglass26@gmail.com>
2023-10-08 11:46:44 +0100

    change to include courses

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -11,7 +11,7 @@ const NavbarRoutes = () => {
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
-  const isVideoPage = pathname?.includes("/chapter");
+  const isVideoPage = pathname?.includes("/courses");
   const isSearchPage = pathname === "/search";
 
   return (
